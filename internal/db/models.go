9c55fc48b7686d604f64c57a6edc5f3a03d602d7
Ackhapocha <prihodko@bmstu.ru>
2026-06-12 16:05:03 +0300

    Запрос авторизации проходит.

--- internal/db/models.go
+++ internal/db/models.go
@@ -3,6 +3,6 @@ package db
 type User struct {
 	Id       int64  `json:"id"`
 	Login    string `json:"login"`
-	Pass     string `json:"pass"`
+	Password string `json:"password"`
 	Is_admin bool   `json:"is_admin"`
 }
