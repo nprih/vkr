f726793b100f63ec7279105f38e3bd222b716844
Ackhapocha <prihodko@bmstu.ru>
2026-06-20 00:10:18 +0300

    Рефакторинг

---
+++ web/js/functions.js
@@ -0,0 +1,92 @@
+function register(action) {
+    let post = {
+        login: $('#registerUsername')[0].value,
+        password: registerPassword[0].value
+    }
+    if (action === 'click' || action === 'keydown') {
+        sendPost(post, '/register')
+    } else {
+        alert("Все поля должны быть заполнены")
+    }
+}
+
+function login(action){
+    let post = {
+        login: $('#loginUsername')[0].value,
+        password: loginPassword[0].value
+    }
+    if (action === 'click' || action === 'keydown') {
+        if (post.login && post.password) {
+            sendPost(post, '/login')
+            loginPassword.val('')
+        } else {
+            alert("Все поля должны быть заполнены")
+        }
+    }
+}
+
+function getImages() {
+    $.get(baseUrl + '/images', function(data) {
+        if (data.images) {
+            if (data.images.length !== 0) {
+                refreshImages(data.images)
+            }
+        }
+    })
+}
+
+function closeLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+    document.body.style.overflow = '';
+}
+
+function sendPost(post, url) {
+    $.ajax({
+        url: url,
+        type: "POST",
+        contentType: 'application/json',
+        data: JSON.stringify(post),
+        dataType: 'json',
+        async: false,
+        success: function(data) {
+            if (data.fail){
+                alert(data.fail)
+            }
+            if (data.redirect){
+                window.location.replace(baseUrl + data.redirect)
+            }
+        }
+    });
+}
+
+function refreshImages(images) {
+    $('#adminPhotosTable').replaceWith(formatImages(images));
+}
+
+function formatImages(images) {
+    let rows = [];
+    let newImages = `
+                    <table id="adminPhotosTable">
+                    <thead>
+                        <tr><th>Превью</th><th>Автор</th><th>Дата</th><th>Действия</th></tr>
+                    </thead>
+                    <tbody id="adminPhotosBody">
+                `
+    $.each(images, function (index, value) {
+        rows.push(`
+                        <tr>
+                            <td id="${value.id}" title="Нажмите для просмотра">
+                                <img class="thumb-mini" src="${value.url}" alt="миниатюра" style="cursor:pointer;">
+                            </td>
+                            <td>${value.author}</td>
+                            <td>${value.createdAt}</td>
+                            <td>
+                            <button class="btn btn-danger btn-sm">🗑 Удалить</button>
+                            </td>
+                        </tr>
+                    `);
+    })
+    newImages += rows.join('');
+    newImages += '</table>';
+    return newImages
+}
--- web/js/main.js
+++ web/js/main.js
@@ -6,10 +6,11 @@ let registerPassword = $('#registerPassword')
 let pathname = $(location).attr('pathname');
 let selected
 
+/** Переход на главную по нажатию на логотип */
 $('.logo').on('click', function () {
     window.location.replace(baseUrl + '/')
 })
-
+/** Смена "активности" вкладок меню */
 $('.nav-link').removeClass('active');
 switch (pathname) {
     case "/":
@@ -30,26 +31,29 @@ switch (pathname) {
 }
 selected.addClass('active');
 
+/** Отправка логин/пароля по нажатию кнопки */
 $('.login').on('click', function () {
     login('click')
 })
-
+/** Отправка логин/пароля по нажатию "Enter" находясь в поле "password" */
 loginPassword.on('keydown', function () {
     if (event.key === 'Enter') {
         login('keydown')
     }
 })
 
+/** Отправка логин/пароля по нажатию кнопки */
 $('.register').on('click', function () {
     register('click')
 })
-
+/** Отправка логин/пароля по нажатию "Enter" находясь в поле "password" */
 registerPassword.on('keydown', function () {
     if (event.key === 'Enter') {
         register('keydown')
     }
 })
 
+/** Отправка запроса на прекращение авторизации */
 $('.logout').on('click', function () {
     let post = {
         action: 'logout',
@@ -57,33 +61,7 @@ $('.logout').on('click', function () {
     sendPost(post, '/logout')
 })
 
-function register(action) {
-    let post = {
-        login: $('#registerUsername')[0].value,
-        password: registerPassword[0].value
-    }
-    if (action === 'click' || action === 'keydown') {
-        sendPost(post, '/register')
-    } else {
-        alert("Все поля должны быть заполнены")
-    }
-}
-
-function login(action){
-    let post = {
-        login: $('#loginUsername')[0].value,
-        password: loginPassword[0].value
-    }
-    if (action === 'click' || action === 'keydown') {
-        if (post.login && post.password) {
-            sendPost(post, '/login')
-            loginPassword.val('')
-        } else {
-            alert("Все поля должны быть заполнены")
-        }
-    }
-}
-
+/** Отправка файла для сохранения */
 $('#fileInput').on('change', function () {
     const fileInput = $(this)[0];
     const file = fileInput.files[0];
@@ -110,67 +88,9 @@ $('#fileInput').on('change', function () {
     });
 });
 
+/** Вызов модалки для просмотра картинки */
 $('.photo-card').on('click', function () {
     document.getElementById('lightboxImg').src = $(this).children('img').attr('src');
     document.getElementById('lightbox').style.display = 'flex';
     document.body.style.overflow = 'hidden';
 })
-
-function getImages() {
-    $.get(baseUrl + '/images', function(data) {
-        if (data.images) {
-            if (data.images.length !== 0) {
-                let newImages = `
-                    <table id="adminPhotosTable">
-                    <thead>
-                        <tr><th>Превью</th><th>Автор</th><th>Дата</th><th>Действия</th></tr>
-                    </thead>
-                    <tbody id="adminPhotosBody">
-                `
-
-                let rows = [];
-                $.each(data.images, function (index, value) {
-                    rows.push(`
-                        <tr>
-                            <td id="${value.id}" title="Нажмите для просмотра">
-                                <img class="thumb-mini" src="${value.url}" alt="миниатюра" style="cursor:pointer;">
-                            </td>
-                            <td>${value.author}</td>
-                            <td>${value.createdAt}</td>
-                            <td>
-                            <button class="btn btn-danger btn-sm">🗑 Удалить</button>
-                            </td>
-                        </tr>
-                    `);
-                })
-                newImages += rows.join('');
-                newImages += '</table>';
-                $('#adminPhotosTable').replaceWith(newImages);
-            }
-        }
-    })
-}
-
-function closeLightbox() {
-    document.getElementById('lightbox').style.display = 'none';
-    document.body.style.overflow = '';
-}
-
-function sendPost(post, url) {
-    $.ajax({
-        url: url,
-        type: "POST",
-        contentType: 'application/json',
-        data: JSON.stringify(post),
-        dataType: 'json',
-        async: false,
-        success: function(data) {
-            if (data.fail){
-                alert(data.fail)
-            }
-            if (data.redirect){
-                window.location.replace(baseUrl + data.redirect)
-            }
-        }
-    });
-}
